a488a366e7d70679b5dcfc561da4f275479d82b2
Helder S Ribeiro <git@agentofuser.com>
2021-12-19 02:07:29 -0400

    wip

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -7,32 +7,45 @@ export default function Index() {
       }}
     >
       <h1>Hockeystick Dev</h1>
+      <h2>
+        Learn how to build full-stack websites on the edge with
+        Remix.run and Cloudflare Workers.
+      </h2>
+      <p>
+        Hi, my name is <strong>Helder</strong>, aka{' '}
+        <a href="https://agentofuser.com">@agentofuser</a>, and this is
+        my ubermeta #LearnInPublic dev log about learning Remix and
+        Cloudflare Workers (and Pages, KV, etc.) while building out
+        this very website and the CMS to edit it, which I plan later on
+        to extract into its own product.
+      </p>
+      <p>
+        Things are pretty bare bones at the moment, but feel free to
+        reach out on twitter or by email and let me know if there's
+        something specific you'd like me to cover in a tutorial.
+      </p>
+      <p>Thanks for stopping by!</p>
+      <p>
+        Cheers,
+        <br />
+        Helder
+      </p>
+      <h3>Contact</h3>
       <ul>
         <li>
+          Twitter:{' '}
           <a
             target="_blank"
-            href="https://remix.run/tutorials/blog"
+            href="https://twitter.com/hockeystickdev"
             rel="noreferrer"
           >
-            15m Quickstart Blog Tutorial
+            @hockeystickdev
           </a>
         </li>
         <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
+          Email:{' '}
+          <a href="mailto:webmaster@hockeystick.dev">
+            webmaster@hockeystick.dev
           </a>
         </li>
       </ul>
